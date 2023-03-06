c21d164a0b480ac6f8db79309fa9712794160d09
Tanuja <tanujakhatun292@gmail.com>
2023-03-06 18:43:12 +0530

    project updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 // import Home from './Component/Home';
-import { useState } from 'react';
 import Counter from './Component/Counter';
 
 function App() {
-  const[countryData,setCountryData]=useState([])
   return (
     <div className="App">
      {/* <Home/> */}
--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -1,14 +1,20 @@
-// import { useState } from "react";
+import { useState } from "react";
 import React from 'react'
 import  "./Home.css";
 export default function Counter() {
-    // const [counter,setCounter]=useState[0];
+    const [counter,setCounter]=useState(0);
+    // function sub(){
+    //     if(counter>=0){
+    //       setCounter=counter+1;
+    //     }
+    // }
   return (
     <div>
       <div className="container">
         <div className='counter_app rounded'>
-        <button type="button" class="btn btn-warning px-3 mx-3"> +</button>
-        <button type="button" class="btn btn-warning px-3 mx-3 "> -</button>
+        <button type="button" class="btn btn-warning px-3 mx-3" onClick={()=>setCounter(counter+1)}> +</button>
+        <h4 className='text-primary '>{counter}</h4>
+      <button type="button" class="btn btn-warning px-3 mx-3 " onClick={()=>setCounter(counter >0 && counter-1)}> -</button>
         </div>
       </div>
     </div>
